9da2fefa1df681471d2d317e224f985d3e450d5d
samikia <kia.sami8@gmail.com>
2023-12-05 00:16:12 +0330

    adding header menu

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+const navBtn=document.querySelector(".nav__btn")
+let navOpen=false
+navBtn.addEventListener("click", function(){
+    if(navOpen){
+        navBtn.classList.remove("nav__btn--open")
+        navOpen=false;
+    }else{
+        navBtn.classList.add("nav__btn--open")
+        navOpen=true
+    }
+})
